b86d8ee474e9c829803508673d7a31d86affbb11
ALIESTRADA <82208754+ALIESTRADA@users.noreply.github.com>
2021-07-30 21:59:34 +0200

    Add files via upload

---
+++ covid-analytics-grupo4-cliente/assets/js/script.js
@@ -0,0 +1,28 @@
+﻿const hostName = 'https://covid-analytics-grupo4.test';
+
+// Pais por fecha
+async function getEntriesPorFecha(fecha) {
+  return await apiCall('GET', hostName + '/api/entries/porfecha/' + fecha);
+};
+
+async function getEntriesPorFechaPais(fecha, pais){
+  return await apiCall('GET', hostName + '/api/entries/porfecha/porcountries' + fecha + pais);
+};
+
+const apiCall = async (method, url) => {
+  let config = {
+    method: method,
+    url: url,
+  }
+
+  const response = await axios(config);
+
+  return response.data;
+}
+
+// Exportación de las funciones
+
+export {
+  getEntriesPorFecha,
+  getEntriesPorFechaPais,
+}
